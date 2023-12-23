813d6a6dc339c0a60458482225b9597e2430bc4a
DTAT1611 <113090251+DTAT1611@users.noreply.github.com>
2023-12-23 08:57:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -0,0 +1,37 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+        private Button btnAllJob;
+        private Button btnPostJob;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        btnAllJob=findViewById(R.id.btn_AllJob);
+        btnPostJob=findViewById(R.id.btn_PostJob);
+
+        btnAllJob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(getApplicationContext(), AllJobActivity.class));
+            }
+        });
+
+        btnPostJob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(getApplicationContext(),PostJobActivity.class));
+            }
+        });
+    }
+}
